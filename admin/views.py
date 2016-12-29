bc9a1541f58aaa254ac6171a97ef389cb1c1698e
qinshuang <qinshuang>
2016-12-29 01:00:30 -0800

    add nigty formwork

--- admin/views.py
+++ admin/views.py
@@ -15,5 +15,18 @@ def index(request):
     return render(request, 'index.html')
 
 
+def sign_in(request):
+    """
+    注册
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        pass
+        return
+    user_name = request.POST.get('user_name', None)
+    password = request.POST.get('password', None)
+
+
 def user_manage(request):
     return render(request, 'index.html')
--- conf/test/settings.py
+++ conf/test/settings.py
@@ -93,6 +93,9 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
